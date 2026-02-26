d3f923089b34537a2181bf63646d4f24208f68af
kuro-spm <sarapratsm@gmail.com>
2026-02-26 01:30:49 +0100

    correccions fitxa.cpp

--- 20260108_VS_Biblioteca_Sara/Fitxa.cpp
+++ 20260108_VS_Biblioteca_Sara/Fitxa.cpp
@@ -8,43 +8,36 @@
 //==================CONSTRUCTORS==================
 
 Fitxa::Fitxa() : referencia("REF"), titol(NULL), esDeixa(NULL), dataCreacio(NULL) {
-	////Inicialitzar les dades dinàmiques a null
-	//this->titol = NULL;
-	//this->esDeixa = NULL;
-
-	/*strcpy(this->referencia, "??????????");
-	this->titol = NULL;
-	this->esDeixa = NULL;*/
-	//setReferencia("??????????");
+	//Necessitem que les dades dinàmiques estiguin inicialitzades a NULL.	
 	setTitol("?");
 }
 
-//Hhem de posar el nom de la classe al principi per indicar que forma part de la classe.
-Fitxa::Fitxa(const char* referencia, const char* titol): esDeixa(NULL), dataCreacio(NULL) {
+//Hem de posar el nom de la classe:: al principi per indicar que forma part de la classe.
+Fitxa::Fitxa(const char* referencia, const char* titol) : esDeixa(NULL), dataCreacio(NULL) {
 	//Inicialitzar les dades dinàmiques a null
-	this->esDeixa = NULL;
-	//Gestio de la referencia
-	if (referencia == NULL || strlen(referencia) != REF_LEN) {
-		throw "La referencia és obligatoria i de llargada REF_LEN";
-	}
-	strcpy(this->referencia, referencia);
+	setReferencia(referencia);
+	setTitol(titol);
 
-	//gestio del titol:
-	if (titol == NULL || strlen(titol) == 0) {
-		throw "El títol és obligatori";
-	}
-	//Creem espai pel titol:
-	this->titol = new char[strlen(titol) + 1];
-	if (this->titol == NULL) {
-		throw "no hi ha memoria pel titol de la fitxa";
-	}
-	strcpy(this->titol, titol);
+	////Gestio de la referencia
+	//if (referencia == NULL || strlen(referencia) != REF_LEN) {
+	//	throw "La referencia és obligatoria i de llargada REF_LEN";
+	//}
+	//strcpy(this->referencia, referencia); //strcpy per les cadenes...
+
+	////gestio del titol:
+	//if (titol == NULL || strlen(titol) == 0) {
+	//	throw "El títol és obligatori";
+	//}
+	////Creem espai pel titol:
+	//this->titol = new char[strlen(titol) + 1];
+	//if (this->titol == NULL) {
+	//	throw "no hi ha memoria pel titol de la fitxa";
+	//}
+	//strcpy(this->titol, titol); //strcpy per les cadenes...
 }
 
-Fitxa::Fitxa(const char* referencia, const char* titol, bool* esDeixa) {
+Fitxa::Fitxa(const char* referencia, const char* titol, bool* esDeixa) : titol(NULL), esDeixa(NULL), dataCreacio(NULL) {
 	//Inicialitzar les dades dinàmiques a null
-	this->titol = NULL;
-	this->esDeixa = NULL;
 
 	//Gestio de la referencia
 	if (referencia == NULL || strlen(referencia) != REF_LEN) {
@@ -68,7 +61,7 @@ Fitxa::Fitxa(const char* referencia, const char* titol, bool* esDeixa) {
 
 
 }
-Fitxa::Fitxa(const char* referencia, const char* titol, bool* esDeixa, Data* dataCreacio)
+Fitxa::Fitxa(const char* referencia, const char* titol, bool* esDeixa, Data* dataCreacio) : titol(NULL), esDeixa(NULL), dataCreacio(NULL)
 {
 	//Inicialitzar les dades dinàmiques a null
 	this->titol = NULL;
@@ -98,12 +91,132 @@ Fitxa::Fitxa(const char* referencia, const char* titol, bool* esDeixa, Data* dat
 
 
 //==================CONSTRUCTOR DE COPIA==================
-Fitxa::Fitxa(const Fitxa& fitxa) : titol(NULL), esDeixa(NULL) {
+Fitxa::Fitxa(const Fitxa& fitxa) : titol(NULL), esDeixa(NULL), dataCreacio(NULL) {
 	setReferencia(fitxa.referencia);
 	setTitol(fitxa.titol);
 	setEsDeixa(fitxa.esDeixa);
 	setDataCreacio(fitxa.dataCreacio);
-	this->dataAlta= fitxa.dataAlta;
+	this->dataAlta = fitxa.dataAlta;
+}
+
+
+
+//==================DESTRUCTOR==================
+Fitxa::~Fitxa() {
+#ifdef DEBUG
+	std::cout << "\t...Alliberant memoria de la fitxa " << this->referencia << "..." << std::endl;
+#endif // DEBUG
+
+
+	delete[] titol;
+	delete esDeixa;
+	delete dataCreacio;
+	//Per si el programador invoca el destructor per netejar l'objecte:
+	titol = NULL;
+	esDeixa = NULL;
+	dataCreacio = NULL;
+}
+
+
+
+//==================SETTERS==================
+void Fitxa::setReferencia(const char* referencia) {
+	if (referencia == NULL || strlen(referencia) != REF_LEN) {
+		throw "La referència és obligatòria i de llargada REF_LEN [10]";
+	}
+	strcpy(this->referencia, referencia);
+}
+
+void Fitxa::setTitol(const char* titol) {
+	if (titol == NULL || strlen(titol) == 0) {
+		throw "El títol és obligatori";
+	}
+	if (this->titol != NULL && strlen(this->titol) == strlen(titol)) {
+		strcpy(this->titol, titol);
+	}
+	else {
+		char* aux = new char[strlen(titol) + 1];
+		if (aux == NULL) {
+			throw "No hi ha memòria pel títol de la fitxa";
+		}
+		delete[]this->titol;
+		this->titol = aux;
+		strcpy(this->titol, titol);
+	}
+}
+
+void Fitxa::setEsDeixa(const bool* esDeixa) {
+	if (esDeixa == NULL) {
+		delete this->esDeixa;
+		this->esDeixa = NULL;
+	}
+	else {
+		if (this->esDeixa == NULL) {
+			this->esDeixa = new bool;
+			//Comprovar que s'ha pogut crear memoria
+			if (this->esDeixa == NULL) {
+				throw "No s'ha pogut reservar memoria";
+			}
+			//this->esDeixa = (bool*)esDeixa; MALAMENT. COPIEM VALORS, NO ADRECES
+			*(this->esDeixa) = *esDeixa;
+
+		}
+		else {
+			*(this->esDeixa) = *esDeixa;
+		}
+
+	}
+}
+
+void Fitxa::setDataCreacio(const Data* dataCreacio)
+{
+	if (dataCreacio == NULL) {
+		delete this->dataCreacio;
+		this->dataCreacio = NULL;
+	}
+	else {
+		if (this->dataCreacio == NULL) {
+			//Com que ja hi ha espai reservat, copiem la data directament.
+			*(this->dataCreacio) = (*dataCreacio);
+		}
+		else {
+			this->dataCreacio = new Data(*dataCreacio);
+			if (this->dataCreacio == NULL) throw "No s'ha pogut reservar memoria";
+		}
+	}
+}
+
+
+//==================GETTERS==================
+const char* Fitxa::getReferencia() const { return this->referencia; }
+const char* Fitxa::getTitol() const { return this->titol; }
+const bool* Fitxa::getEsDeixa() const { return this->esDeixa; }
+const Data* Fitxa::getDataCreacio() const { return this->dataCreacio; }
+const Data& Fitxa::getDataAlta() const { return this->dataAlta; }
+
+
+//==================METODES==================
+void Fitxa::visualitzar() {
+	std::cout << "  Referencia: " << this->getReferencia() << std::endl;
+	std::cout << "  Titol: " << this->getTitol() << std::endl;
+	std::cout << "  Es deixa?: ";
+	if (this->esDeixa == NULL) {
+		std::cout << "?" << std::endl;;
+	}
+	else if (*(this->esDeixa)) {
+		std::cout << "Si" << std::endl;
+	}
+	else {
+		std::cout << "No" << std::endl;
+	}
+	std::cout << "  Data d'alta: " << this->dataAlta << std::endl;;;
+	std::cout << "  Data creació: ";
+	if (this->dataCreacio == NULL) {
+		std::cout << "?" << std::endl;;
+	}
+	else {
+		std::cout << *(this->dataCreacio) << std::endl;;
+	}
 }
 
 //==================OPERADORS==================
@@ -153,137 +266,16 @@ bool Fitxa::operator>(const Fitxa& fitxa)
 
 
 
-//==================DESTRUCTOR==================
-Fitxa::~Fitxa() {
-#ifdef DEBUG
-	std::cout << "\t...Alliberant memoria de la fitxa " << this->referencia << "..." << std::endl;
-#endif // DEBUG
-
-
-	delete[] titol;
-	delete esDeixa;
-	delete dataCreacio;
-	//Per si el programador invoca el destructor per netejar l'objecte:
-	titol = NULL;
-	esDeixa = NULL;
-	dataCreacio = NULL;
-}
-
-
-
-//==================SETTERS==================
-void Fitxa::setReferencia(const char* referencia) {
-	if (referencia == NULL || strlen(referencia) != REF_LEN) {
-		throw "La referència és obligatòria i de llargada REF_LEN [10]";
-	}
-
-	// Omplim la referència
-	strcpy(this->referencia, referencia);
-}
-
-void Fitxa::setTitol(const char* titol) {
-	if (titol == NULL || strlen(titol) == 0) {
-		throw "El títol és obligatori";
-	}
-	//Hem d'alliberar el titol anterior
-	delete[] this->titol;
-	//SI volguessim més eficiencia podem comprovar si la length del titol anterior es igual a l'actual.
-
-	// Omplim el títol dinàmic
-	this->titol = new char[strlen(titol) + 1];
-	if (this->titol == NULL) {
-		throw "No hi ha memòria pel títol de la fitxa";
-	}
-
-	strcpy(this->titol, titol);
-}
-
-void Fitxa::setEsDeixa(const bool* esDeixa) {
-	if (esDeixa == NULL) {
-		delete this->esDeixa;
-		this->esDeixa = NULL;
-	}
-	else {
-		if (this->esDeixa == NULL) {
-			this->esDeixa = new bool;
-			//Comprovar que s'ha pogut crear memoria
-			if (this->esDeixa == NULL) {
-				throw "No s'ha pogut reservar memoria";
-			}
-			//this->esDeixa = (bool*)esDeixa; MALAMENT. COPIEM VALORS, NO ADRECES
-			*(this->esDeixa) = *esDeixa;
-
-		}
-		else {
-			*(this->esDeixa) = *esDeixa;
-		}
-
-	}
-}
-
-void Fitxa::setDataCreacio(const Data* dataCreacio)
-{
-	if (dataCreacio == NULL) {
-		delete this->dataCreacio  ;
-		this->dataCreacio = NULL;
-	}
-	else {
-		if (this->dataCreacio == NULL) {
-			this->dataCreacio = new Data(*dataCreacio);
-			if (this->esDeixa == NULL) {
-				throw "No s'ha pogut reservar memoria";
-			}
-			*(this->dataCreacio) = *dataCreacio;
-		}
-		else {
-			*(this->dataCreacio) = *dataCreacio;
-		}
-	}
-
-}
-
-
-//==================GETTERS==================
-const char* Fitxa::getReferencia() const { return this->referencia; }
-const char* Fitxa::getTitol() const { return this->titol; }
-const bool* Fitxa::getEsDeixa() const { return this->esDeixa; }
-const Data* Fitxa::getDataCreacio() const { return this->dataCreacio; }
-const Data& Fitxa::getDataAlta() const { return this->dataAlta; }
-
-
-//==================METODES==================
-void Fitxa::visualitzar() {
-	std::cout << "  Referencia: " << this->getReferencia() << std::endl;
-	std::cout << "  Titol: " << this->getTitol() << std::endl;
-	std::cout << "  Es deixa?: ";
-	if (this->esDeixa == NULL) {
-		std::cout << "?" << std::endl;;
-	}
-	else if (*(this->esDeixa)) {
-		std::cout << "Si" << std::endl;
-	}
-	else {
-		std::cout << "No" << std::endl;
-	}
-	std::cout << "  Data d'alta: "<< this->dataAlta << std::endl;;;
-	std::cout << "  Data creació: ";
-	if (this->dataCreacio == NULL) {
-		std::cout << "?" << std::endl;;
-	}
-	else {
-		std::cout << *(this->dataCreacio) << std::endl;;
-	}
-
-
-}
-
 ostream& operator<<(ostream& os, const Fitxa& fitxa)
 {
 	//Això és ineficient
 	//os << "Ref: " << fitxa.getReferencia() << "- Titol: " << fitxa.getTitol();
-	
+
 	//Farem les classes friends per no haver de cridar els getters.
 	//(Hem posat 	friend ostream& operator<<(ostream& os, Fitxa& fitxa); al fitxer.h)
+
+	//.h :     friend ostream &operator<<(ostream &os, const Fitxa &fitxa);
+
 	os << "Ref: " << fitxa.referencia << " - Titol: " << fitxa.titol;
 
 	return os;
